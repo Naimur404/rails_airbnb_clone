65f42fa63299624f229ce89618773fe8c587f6ae
naimur404 <naimur404cse@gmail.com>
2024-06-20 23:15:42 +0600

    1st commit

---
+++ app/models/property.rb
@@ -0,0 +1,14 @@
+class Property < ApplicationRecord
+
+
+  validates :name, presence: true
+  validates :headline, presence: true
+  validates :description, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :address_1, presence: true
+  validates :country, presence: true
+  monetize :price_cents, allow_nil: true
+
+  has_many_attached :images
+end
---
+++ config/tailwind.config.js
@@ -0,0 +1,31 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+    colors: {
+      ...colors,
+      primary: {
+        DEFAULT: '#FF5A5F',
+        dark: '#FF385C',
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
+  ]
+}
